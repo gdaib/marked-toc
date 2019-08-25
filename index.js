97d76fe5628063dbe86fb1c87ad684edb77e55ac
cjfff <865553742@qq.com>
2019-08-25 13:44:26 +0800

    feat: fix

--- index.js
+++ index.js
@@ -5,22 +5,21 @@
  * Licensed under the MIT license.
  */
 
-'use strict';
+"use strict";
 
-var marked = require('marked');
-// var matter = require('gray-matter');
-var template = require('template');
-var slugify = require('uslug');
-var _ = require('lodash');
-var utils = require('./lib/utils').default;
+var marked = require("marked");
+var slugify = require("uslug");
+var _ = require("lodash");
+var utils = require("./lib/utils").default;
 
 /**
  * Default template to use for generating
  * a table of contents.
  */
 
-var defaultTemplate = '<%= depth %><%= bullet %>[<%= heading %>](#<%= url %>)\n';
+const data2String = `{depth}{bullet}{heading}(#{url})\n`;
 
+const template = data => data2String.replace(/{(.*?)}/g, (_, key) => data[key]);
 
 /**
  * Create the table of contents object that
@@ -32,83 +31,86 @@ var defaultTemplate = '<%= depth %><%= bullet %>[<%= heading %>](#<%= url %>)\n'
  */
 
 function generate(str, options) {
-  var opts = _.extend({
-    firsth1: false,
-    blacklist: true,
-    omit: [],
-    maxDepth: 3,
-    slugifyOptions: { allowedChars: '-' },
-    slugify: function(text) {
-      return slugify(text, opts.slugifyOptions);
-    }
-  }, options);
+  var opts = _.extend(
+    {
+      firsth1: false,
+      blacklist: true,
+      omit: [],
+      maxDepth: 3,
+      slugifyOptions: { allowedChars: "-" },
+      slugify: function(text) {
+        return slugify(text, opts.slugifyOptions);
+      }
+    },
+    options
+  );
 
-  var toc = '';
+  var toc = "";
   var tokens = marked.lexer(str);
   var tocArray = [];
 
   // Remove the very first h1, true by default
-  if(opts.firsth1 === false) {
+  if (opts.firsth1 === false) {
     tokens.shift();
   }
 
   // Do any h1's still exist?
-  var h1 = _.any(tokens, {depth: 1});
+  var h1 = _.any(tokens, { depth: 1 });
 
-  tokens.filter(function (token) {
-    // Filter out everything but headings
-    if (token.type !== 'heading' || token.type === 'code') {
-      return false;
-    }
+  tokens
+    .filter(function(token) {
+      // Filter out everything but headings
+      if (token.type !== "heading" || token.type === "code") {
+        return false;
+      }
 
-    // Since we removed the first h1, we'll check to see if other h1's
-    // exist. If none exist, then we unindent the rest of the TOC
-    if(!h1) {
-      token.depth = token.depth - 1;
-    }
+      // Since we removed the first h1, we'll check to see if other h1's
+      // exist. If none exist, then we unindent the rest of the TOC
+      if (!h1) {
+        token.depth = token.depth - 1;
+      }
 
-    // Store original text and create an id for linking
-    token.heading = opts.clean ? utils.clean(token.text, opts) : token.text;
+      // Store original text and create an id for linking
+      token.heading = opts.clean ? utils.clean(token.text, opts) : token.text;
 
-    // Create a "slugified" id for linking
-    token.id = opts.slugify(token.text);
+      // Create a "slugified" id for linking
+      token.id = opts.slugify(token.text);
 
-    // Omit headings with these strings
-    var omissions = ['Table of Contents', 'TOC', 'TABLE OF CONTENTS'];
-    var omit = _.union([], opts.omit, omissions);
+      // Omit headings with these strings
+      var omissions = ["Table of Contents", "TOC", "TABLE OF CONTENTS"];
+      var omit = _.union([], opts.omit, omissions);
 
-    if (utils.isMatch(omit, token.heading)) {
-      return;
-    }
+      if (utils.isMatch(omit, token.heading)) {
+        return;
+      }
 
-    return true;
-  }).forEach(function (h) {
+      return true;
+    })
+    .forEach(function(h) {
+      if (h.depth > opts.maxDepth) {
+        return;
+      }
 
-    if(h.depth > opts.maxDepth) {
-      return;
-    }
+      var bullet = Array.isArray(opts.bullet)
+        ? opts.bullet[(h.depth - 1) % opts.bullet.length]
+        : opts.bullet;
 
-    var bullet = Array.isArray(opts.bullet) ? opts.bullet[(h.depth - 1) % opts.bullet.length] : opts.bullet;
+      var data = _.extend({}, opts.data, {
+        depth: new Array((h.depth - 1) * 2 + 1).join(" "),
+        bullet: bullet ? bullet : "* ",
+        heading: h.heading,
+        url: h.id
+      });
 
-    var data = _.extend({}, opts.data, {
-      depth  : new Array((h.depth - 1) * 2 + 1).join(' '),
-      bullet : bullet ? bullet : '* ',
-      heading: h.heading,
-      url    : h.id
+      tocArray.push(data);
+      toc += template(data);
     });
-
-    tocArray.push(data);
-
-    var tmpl = opts.template || defaultTemplate;
-    toc += template(tmpl, data);
-  });
 
   return {
     data: tocArray,
     toc: opts.clean ? utils.clean(toc, opts) : toc
   };
 }
-
 
 /**
  * toc
@@ -118,11 +120,9 @@ function toc(str, options) {
   return generate(str, options).toc;
 }
 
-
 toc.raw = function(str, options) {
   return generate(str, options);
 };
-
 
 // toc.insert = function(str, options) {
 //   var start = '<!-- toc -->';
@@ -139,7 +139,6 @@ toc.raw = function(str, options) {
 //   var table = '\n\n' + start + '\n\n' + toc(content, options) + '\n' + stop + '\n';
 //   return front + content.replace(start, table);
 // };
-
 
 /**
  * Expose `toc`
